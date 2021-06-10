80bc7b43515bd939cf80aab364695b152f86fe2d
hotire <gngh0101@gmail.com>
2021-06-10 22:15:35 +0900

    Add HelloController

---
+++ src/main/java/com/github/hotire/spring/vue/HelloController.java
@@ -0,0 +1,15 @@
+package com.github.hotire.spring.vue;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
+}
